47ca31ed24701d226ebb21c250f88939ad23d874
SudhanshuGupta26 <guptasudhanshukumar1@gmail.com>
2026-02-21 16:42:37 +0000

    Account Abstraction Contract Coded!

---
+++ README.md
@@ -0,0 +1,6 @@
+# About
+1. Create a basic AA on ethereum.
+2. Create a basic AA on zksyn.
+3. Deploy and send a userOp / transaction through them
+    1. We are not going to send an AA to Ethereum.
+    2. But we will send an AA tx to zksync.
---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IAccount} from "../../lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "../../lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "../../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "../../lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "../../lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "../../lib/account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "../../lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+
+// struct PackedUserOperation {
+//     address sender;      // our minimal account
+//     uint256 nonce;       // nonce
+//     bytes initCode;      // ignore
+//     bytes callData;      // this is where we put "the good stuff"
+//     bytes32 accountGasLimits;
+//     uint256 preVerificationGas;
+//     bytes32 gasFees;
+//     bytes paymasterAndData;
+//     bytes signature;
+// }
+
+contract MinimalAccount is IAccount, Ownable {
+    error MinimalAccount__NotFromEntryPoint();
+    error MinimalAccount__NotFromEntryPointOrOwner();
+    error MinimalAccount__CallFailed(bytes);
+
+    IEntryPoint private immutable i_entryPoint;
+
+    modifier requireFromEntryPoint() {
+        if (msg.sender != address(i_entryPoint)) {
+            revert MinimalAccount__NotFromEntryPoint();
+        }
+        _;
+    }
+
+    modifier requireFromEntryPointOrOwner() {
+        if (msg.sender != address(i_entryPoint) && msg.sender != owner()) {
+            revert MinimalAccount__NotFromEntryPointOrOwner();
+        }
+        _;
+    }
+
+    constructor(address entryPoint) Ownable(msg.sender) {
+        i_entryPoint = IEntryPoint(entryPoint);
+    }
+
+    receive() external payable {} // So that this contracct can hold funds in case there is no paymaster
+
+    ///////////////////////////////////
+    //      External Functions       //
+    ///////////////////////////////////
+
+    function execute(
+        address dest,
+        uint256 value,
+        bytes calldata functionData
+    ) external requireFromEntryPoint {
+        (bool success, bytes memory result) = dest.call{value: value}(
+            functionData
+        );
+        if (!success) {
+            revert MinimalAccount__CallFailed(result);
+        }
+    }
+
+    // A signature is valid, if it's the MinimalAccount owner.
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external requireFromEntryPoint returns (uint256 validationData) {
+        uint256 validationData = _validateSignature(userOp, userOpHash);
+        // !!! Here is no validation for nonce, but we should also check this.
+
+        _payPrefund(missingAccountFunds);
+    }
+
+    ///////////////////////////////////
+    //      Internal Functions       //
+    ///////////////////////////////////
+
+    // EIP-191 version of the signed hash
+    function _validateSignature(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash
+    ) internal view returns (uint256 validationData) {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(
+            userOpHash
+        );
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds != 0) {
+            (bool success, ) = payable(msg.sender).call{
+                value: missingAccountFunds,
+                gas: type(uint256).max
+            }("");
+            (success);
+        }
+    }
+
+    ///////////////////////////////////
+    //           Getters             //
+    ///////////////////////////////////
+
+    function getEntryPoint() external view returns (address) {
+        return address(i_entryPoint);
+    }
+}
